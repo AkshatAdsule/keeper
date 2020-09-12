4faf7530d0c80a4a627222922e96c01741b0f07a
akshatadsule <akshat.adsule@gmail.com>
2020-09-11 23:52:37 -0700

    Create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+    const current_year = new Date().getFullYear();
+    return(
+        <footer className="footer">
+            <p>Copyright Akshat Adsule {current_year}</p>
+        </footer>
+    );
+}
+
+export default Footer;
