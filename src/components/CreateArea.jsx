869c4e7bf4e57e9d8ce2e8660f4380f86ac57ba0
akshatadsule <akshat.adsule@gmail.com>
2020-10-10 12:52:56 -0700

    Add material-ui icons and expanding UI

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,42 +1,54 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
   const [headingText, setHeadingText] = useState("");
   const [bodyText, setBodyText] = useState("");
+  const [enabled, setEnabled] = useState(false);
+
   return (
     <div>
-      <form>
-        <input
-          value={headingText}
-          onChange={(event) => setHeadingText(event.target.value)}
-          name="title"
-          placeholder="Title"
-          autoComplete="off"
-        />
+      <form className="create-note">
+        {enabled && (
+          <input
+            value={headingText}
+            onChange={(event) => setHeadingText(event.target.value)}
+            name="title"
+            placeholder="Title"
+            autoComplete="off"
+          />
+        )}
         <textarea
           value={bodyText}
           onChange={(event) => setBodyText(event.target.value)}
+          onClick={() => setEnabled(true)}
           name="content"
           placeholder="Take a note..."
-          rows="3"
+          rows={enabled ? 3 : 1}
         />
-        <button
-          onClick={(e) => {
-            const id = uuidv4();
-            props.onAdd({
-              key: id,
-              id: id,
-              title: headingText,
-              content: bodyText,
-            });
-            setHeadingText("");
-            setBodyText("");
-            e.preventDefault();
-          }}
-        >
-          Add
-        </button>
+        {enabled && (
+          <Zoom in="true">
+            <Fab
+              onClick={(e) => {
+                const id = uuidv4();
+                props.onAdd({
+                  key: id,
+                  id: id,
+                  title: headingText,
+                  content: bodyText,
+                });
+                setHeadingText("");
+                setBodyText("");
+                e.preventDefault();
+              }}
+            >
+              <AddIcon />
+            </Fab>
+          </Zoom>
+        )}
       </form>
     </div>
   );
